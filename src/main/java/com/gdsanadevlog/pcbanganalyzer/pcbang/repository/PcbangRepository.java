884f96aea94a994053fd04f6ad006067a3af78c4
tgoddessana <twicegoddessana1229@gmail.com>
2024-07-04 09:26:04 +0900

    Feat: PC방 리포지토리 기본 생성

---
+++ src/main/java/com/gdsanadevlog/pcbanganalyzer/pcbang/repository/PcbangRepository.java
@@ -0,0 +1,7 @@
+package com.gdsanadevlog.pcbanganalyzer.pcbang.repository;
+
+import com.gdsanadevlog.pcbanganalyzer.pcbang.domain.Pcbang;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PcbangRepository extends JpaRepository<Pcbang, Long>{
+}
