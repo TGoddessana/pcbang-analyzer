d4181dce6b2e535dc0eec664aa957d54fb98381c
hashi285 <m51187251@gmail.com>
2024-07-05 14:21:22 +0900

    수정: Java 버전 호환성 추가

--- src/main/java/com/gdsanadevlog/pcbanganalyzer/auth/controller/AuthController.java
+++ src/main/java/com/gdsanadevlog/pcbanganalyzer/auth/controller/AuthController.java
@@ -1,35 +1,30 @@
 package com.gdsanadevlog.pcbanganalyzer.auth.controller;
 
+import com.gdsanadevlog.pcbanganalyzer.auth.domain.Admin;
+import com.gdsanadevlog.pcbanganalyzer.auth.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.gdsanadevlog.pcbanganalyzer.auth.service.LoginService;
-import com.gdsanadevlog.pcbanganalyzer.auth.service.LogoutService;
 
 
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
-    private final LoginService loginService;
-    private final LogoutService logoutService;
+    private final AdminService adminService;
 
     @GetMapping("/login")
-    public String login() {
-        // 로그인 폼을 보여주는 뷰
+    public String userregform(){
         return "pages/auth/login";
     }
 
-    @PostMapping("/login")
-    public String loginPost() {
-        // 로그인 처리
-        return "redirect:/";
+
+    @GetMapping("/index")
+    public String welcome(){
+        return "pages/index";
     }
 
-    @PostMapping("/logout")
-    public String logout() {
-        // 로그아웃 처리
-        return "redirect:/";
-    }
 }
--- src/main/java/com/gdsanadevlog/pcbanganalyzer/auth/service/LoginService.java
+++
@@ -1,7 +0,0 @@
-package com.gdsanadevlog.pcbanganalyzer.auth.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class LoginService {
-}
