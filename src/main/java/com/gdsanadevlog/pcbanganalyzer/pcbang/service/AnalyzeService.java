e8e994c16203a977ea1e2ff36bd78cdac73c4aad
tgoddessana <twicegoddessana1229@gmail.com>
2024-07-19 18:36:51 +0900

    Feat:

--- src/main/java/com/gdsanadevlog/pcbanganalyzer/pcbang/service/AnalyzeService.java
+++ src/main/java/com/gdsanadevlog/pcbanganalyzer/pcbang/service/AnalyzeService.java
@@ -1,14 +1,22 @@
 package com.gdsanadevlog.pcbanganalyzer.pcbang.service;
 
 import com.gdsanadevlog.pcbanganalyzer.pcbang.domain.AnalyzeHistory;
+import com.gdsanadevlog.pcbanganalyzer.pcbang.domain.Pcbang;
 import com.gdsanadevlog.pcbanganalyzer.pcbang.dto.AnalyzeHistoryReadDto;
 import com.gdsanadevlog.pcbanganalyzer.pcbang.repository.AnalyzeHistoryRepository;
 
+import com.gdsanadevlog.pcbanganalyzer.pcbang.repository.PcbangRepository;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 
 
 @Service
@@ -16,6 +24,7 @@ import org.springframework.stereotype.Service;
 public class AnalyzeService {
 
     private final AnalyzeHistoryRepository analyzeHistoryRepository;
+    private final PcbangRepository pcbangRepository;
 
     public Page<AnalyzeHistoryReadDto> listAnalyzeHistoriesPaginated(int page, int size) {
         Page<AnalyzeHistory> analyzeHistoryPage = analyzeHistoryRepository.findAll(PageRequest.of(page, size));
@@ -24,16 +33,16 @@ public class AnalyzeService {
     }
 
 //    @Scheduled(cron = "0 0/30 * * * *")
-//    @Async
-//    @Scheduled(cron = "0/10 * * * * *")
-//    public void analyzePcbang() {
-//        System.out.println("Analyze PCBangs at " + LocalDateTime.now());
-//        List<Pcbang> pcbangs = pcbangRepository.findAll();
-//
-//        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
-//        for (Pcbang pcbang : pcbangs) {
-//            AnalyzeHistory analyzeHistory = pcbang.analyze(now);
-//            analyzeHistoryRepository.save(analyzeHistory);
-//        }
-//    }
+    @Async
+    @Scheduled(cron = "0/10 * * * * *")
+    public void analyzePcbang() {
+        System.out.println("Analyze PCBangs at " + LocalDateTime.now());
+        List<Pcbang> pcbangs = pcbangRepository.findAll();
+
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        for (Pcbang pcbang : pcbangs) {
+            AnalyzeHistory analyzeHistory = pcbang.analyze(now);
+            analyzeHistoryRepository.save(analyzeHistory);
+        }
+    }
 }
