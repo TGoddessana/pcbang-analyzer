4a383e8619a520dc88e0715bb2408318c79b3fa3
tgoddessana <twicegoddessana1229@gmail.com>
2024-07-28 23:11:14 +0900

    배포용 스크립트 작성

--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Stop and remove existing containers
 docker-compose down
 
+# Remove unused images
 docker image prune -a -f
 
+# Pull the latest code from git
+git pull origin main
+
+# Build and start new containers
 docker-compose up --build -d
 
+# Remove dangling images
 docker image prune -f
