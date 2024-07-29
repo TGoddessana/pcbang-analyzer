ab631b8f0e4fc09678de4f8cce42860e763f99ab
tgoddessana <twicegoddessana1229@gmail.com>
2024-07-29 15:49:13 +0900

    배포 스크립트 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ poetry run python manage.py runserver 0.0.0.0:8000 &
 
 poetry run python manage.py migrate
 
+poetry run python manage.py createsuperuser --noinput
+
 poetry run python manage.py collectstatic --noinput
 
 poetry run celery -A config worker --loglevel=info &
