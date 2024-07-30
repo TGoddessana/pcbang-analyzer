7760dfb4fb2527836ae25daf66e187bf618d3998
tgoddessana <twicegoddessana1229@gmail.com>
2024-07-31 00:56:22 +0900

    docker-compose 명령어 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-poetry run python manage.py migrate
-
-poetry run python manage.py collectstatic --noinput
-
-poetry run celery -A config worker --loglevel=info &
-
-poetry run celery -A config beat --loglevel=info &
-
-poetry run python manage.py runserver 0.0.0.0:8000
+if [ "$1" = "bash" ]; then
+  exec "$@"
+else
+  poetry run python manage.py migrate
+  poetry run python manage.py collectstatic --noinput
+  poetry run celery -A config worker --loglevel=info &
+  poetry run celery -A config beat --loglevel=info &
+  exec poetry run python manage.py runserver 0.0.0.0:8000
+fi
